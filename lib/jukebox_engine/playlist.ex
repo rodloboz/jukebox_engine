978a1faf621544713e1527eea53809226fef69c1
Rui Freitas <rui.freitas@hopin.to>
2022-08-22 22:17:31 +0100

    User List for playlist songs

--- lib/jukebox_engine/playlist.ex
+++ lib/jukebox_engine/playlist.ex
@@ -3,31 +3,19 @@ defmodule JukeboxEngine.Playlist do
 
   alias JukeboxEngine.{Playlist, Song}
 
-  @enforce_keys [:songs, :played_songs, :current_song, :next_song]
-  defstruct [:songs, :played_songs, :current_song, :next_song]
+  @enforce_keys [:songs, :played_songs]
+  defstruct [:songs, :played_songs]
 
   def new do
     %Playlist{
-      songs: MapSet.new(),
-      played_songs: MapSet.new(),
-      current_song: nil,
-      next_song: nil
+      songs: [],
+      played_songs: []
     }
   end
 
   def add_song(playlist, %Song{} = song) do
-    songs = MapSet.put(playlist.songs, song)
+    songs = [song | playlist.songs]
 
-    case playlist.next_song do
-      nil -> %{playlist | songs: songs, next_song: song}
-      _ -> %{playlist | songs: songs}
-    end
-  end
-
-  def remove_song(playlist, %Song{} = song) do
-    songs = MapSet.delete(playlist.songs, song)
-    played_songs = MapSet.delete(playlist.played_songs, song)
-
-    %{playlist | songs: songs, played_songs: played_songs}
+    %{playlist | songs: songs}
   end
 end
--- test/jukebox_engine/playlist_test.exs
+++ test/jukebox_engine/playlist_test.exs
@@ -12,42 +12,11 @@ defmodule JukeboxEngine.PlaylistTest do
 
   describe "add_song/2" do
     test "should add song to the playlist", %{playlist: playlist, song: song} do
-      refute MapSet.member?(playlist.songs, song)
+      refute Enum.member?(playlist.songs, song)
 
       playlist = Playlist.add_song(playlist, song)
 
-      assert MapSet.member?(playlist.songs, song)
-    end
-
-    test "should not add song twice to the playlist", %{playlist: playlist, song: song} do
-      playlist = Playlist.add_song(playlist, song)
-      assert MapSet.size(playlist.songs) == 1
-
-      playlist = Playlist.add_song(playlist, song)
-      assert MapSet.size(playlist.songs) == 1
-    end
-
-    test "should add the song as the next_song when the playlist is empty", %{
-      playlist: playlist,
-      song: song
-    } do
-      refute(playlist.next_song)
-
-      playlist = Playlist.add_song(playlist, song)
-
-      assert playlist.next_song == song
-    end
-
-    test "should not add the song as the next_song when the playlist is not empty", %{
-      playlist: playlist,
-      song: song
-    } do
-      another_song = Song.new("Prior song", 5.40, "https://somesource.com")
-
-      playlist = Playlist.add_song(playlist, another_song)
-      playlist = Playlist.add_song(playlist, song)
-
-      refute playlist.next_song == song
+      assert Enum.member?(playlist.songs, song)
     end
   end
 end
